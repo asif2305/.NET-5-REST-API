891adcd83dddc74170fc3ed6471d6a486033084b
Asif Ahmed <mygithub2305@gmail.com>
2022-05-15 16:53:35 +0200

    Eample of Button

---
+++ WpfApp/ButtonDemo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double myFontSize = myLabel.FontSize; 
+            myLabel.FontSize = myFontSize +1;
+        }
+
+        private void myBUtton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            double myFontSize = myLabel.FontSize;
+            myLabel.FontSize = myFontSize - 1;
+        }
+
+        private void myBUtton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            myLabel.Foreground = Brushes.Green;
+        }
+
+        private void myBUtton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            myLabel.Foreground = Brushes.Black;
+        }
+    }
+}
