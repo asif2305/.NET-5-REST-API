02a3f1817372551beba0e5ecd82624dcb5c9c97b
Asif Ahmed <mygithub2305@gmail.com>
2022-05-15 12:29:45 +0200

    Working on TextBlock

--- WpfApp/TextBlock/MainWindow.xaml.cs
+++ WpfApp/TextBlock/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TextBlock
+namespace TextBlockDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +24,27 @@ namespace TextBlock
         public MainWindow()
         {
             InitializeComponent();
+            myTextBlock.Text = "Hello from the cs side!";
+            myTextBlock.Foreground = Brushes.Blue;
+            
+            // create text box in the class file
+            TextBlock myTb = new TextBlock();
+            myTb.Text = "Hello new text";
+            myTb.Inlines.Add(" This is inline text This is inline text This is inline text");
+            myTb.Inlines.Add(new Run(" Runtext what needed ")
+            {
+                Foreground = Brushes.Red,
+                TextDecorations = TextDecorations.Underline
+            }); ;
+            myTb.TextWrapping = TextWrapping.Wrap;
+            myTb.Foreground = Brushes.BurlyWood;
+            this.Content = myTb;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender,RequestNavigateEventArgs e)
+        {
+            //System.Diagnostics.Process.Start("cmd", "/c start http://www.google.com");
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
         }
     }
 }
