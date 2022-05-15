70b6ae0e21d12b15bfd0ec6770b7285f98f13810
Asif Ahmed <mygithub2305@gmail.com>
2022-05-16 00:40:56 +0200

    Checkbox implementation

---
+++ WpfApp/CheckBoxDemo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBoxDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbCheese_Checked(object sender, RoutedEventArgs e)
+        {
+            cbCheese.Background = Brushes.Red;
+        }
+
+        private void cbCheese_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cbCheese.Background = Brushes.White;
+        }
+
+        private void cbParent_Checked_Changed(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbParent.IsChecked == true);
+            cbCheese.IsChecked = newVal;
+            cbHam.IsChecked = newVal;
+            cbTuna.IsChecked = newVal;
+            cbPepp.IsChecked = newVal;
+        }
+        private void cbTopingsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbParent.IsChecked = null;
+            if ((cbCheese.IsChecked == true) && (cbHam.IsChecked == true) 
+                && (cbTuna.IsChecked == true) && (cbPepp.IsChecked == true)) { 
+              cbParent.IsChecked = true;
+            }
+            if ((cbCheese.IsChecked == false) && (cbHam.IsChecked == false)
+                && (cbTuna.IsChecked == false) && (cbPepp.IsChecked == false))
+            {
+                cbParent.IsChecked = false;
+            }
+        }
+
+        
+    }
+}
--- WpfApp/TextBlock/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WpfApp/TextBlock/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace TextBlockDemo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TextBlockDemo;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TextBlockDemo;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
