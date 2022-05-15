bdbf56bce21af7c04aa1af5ca542bbc18eec8df7
Asif Ahmed <mygithub2305@gmail.com>
2022-05-15 18:55:43 +0200

    Example of Radio Button

---
+++ WpfApp/RadioButtonDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButtonDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void rbH_Checked(object sender, RoutedEventArgs e)
+        {
+            rbLblP.Background = Brushes.Aquamarine;
+        }
+
+        private void rbH_Unchecked(object sender, RoutedEventArgs e)
+        {
+            rbLblP.Background = Brushes.White;
+        }
+    }
+}
