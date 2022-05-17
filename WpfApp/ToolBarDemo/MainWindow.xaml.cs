44e9b94ad1cd70f834765b963b1b6a49a3657166
Asif Ahmed <mygithub2305@gmail.com>
2022-05-17 16:31:17 +0200

    ToolBarDemo example

---
+++ WpfApp/ToolBarDemo/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToolBarDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.Text = "";
+        }
+
+        private void ComboBox_Selected(object sender, RoutedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem item = comboBox.SelectedItem as ComboBoxItem;
+            string newFontSize = (string)item.Content;
+            int temp;
+
+            if(Int32.TryParse(newFontSize, out temp))
+            {
+                if(myTextBox.Text != null)
+                {
+                    myTextBox.FontSize = temp;
+                }
+            }
+        }
+    }
+}
