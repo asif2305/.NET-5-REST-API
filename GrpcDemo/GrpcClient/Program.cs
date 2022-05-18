21889880f77470c3e7e16174a2eb7e3bfbbab27b
Asif Ahmed <mygithub2305@gmail.com>
2022-05-18 14:15:09 +0200

    Implementation  of gRPC client and server application

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Tim" };
+            var channel = GrpcChannel.ForAddress("https://localhost:7078");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine(reply.Message);
+
+            // channel = GrpcChannel.ForAddress("https://localhost:7078");
+           var customerClient = new Customer.CustomerClient(channel);
+           var clientRequested = new CustomerLookModel { UserId = 1};
+           var customer =  await customerClient.GetCustomerInfoAsync(clientRequested);
+            
+           Console.WriteLine($"{customer.FirstName}{customer.LastName}");
+            Console.WriteLine();
+            Console.WriteLine("New Customer List ");
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"{currentCustomer.FirstName} :" +
+                        $"  {currentCustomer.LastName} {currentCustomer.EmailAddress} : {currentCustomer.Age} :" +
+                        $"{currentCustomer.IsAlive}");
+                }
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ GrpcDemo/GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,72 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "A";
+                output.LastName = "B";
+            }
+            else if (request.UserId == 2) {
+                output.FirstName = "C";
+                output.LastName = "D";
+            }
+            else
+            {
+                output.FirstName = "E";
+                output.LastName = "F";
+            }
+            return Task.FromResult(output);
+             
+        }
+        public override async Task GetNewCustomers(NewCustomerRequest request,
+             IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+             new CustomerModel
+             {
+                 FirstName = "Tim",
+                 LastName = "Corey",
+                 EmailAddress = "tim@gmail.com",
+                 Age = 41,
+                 IsAlive = true,
+             },
+             new CustomerModel
+             {
+                 FirstName = "Jam",
+                 LastName = "Hang",
+                 EmailAddress = "jam@gmail.com",
+                 Age = 43,
+                 IsAlive = false,
+             },
+             new CustomerModel
+             {
+                 FirstName = "Ali",
+                 LastName = "Basat",
+                 EmailAddress = "ali@gmail.com",
+                 Age = 20,
+                 IsAlive = false,
+             }
+            };
+            foreach (var custom in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(custom);
+
+            }
+        }
+    }
+}
