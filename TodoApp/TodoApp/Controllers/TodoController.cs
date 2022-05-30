904b2eaed7ce74d15f01f90b2e05507ec04d7828
Asif Ahmed <mygithub2305@gmail.com>
2022-05-30 15:21:39 +0200

    updated

---
+++ TodoApp/TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Controllers
+{
+    [Route("api/[controller]")] // api/todo
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+       
+    }
+}
